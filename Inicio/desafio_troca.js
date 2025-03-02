1ce47e656219ae16159f28a1c4743a4cc94077a4
Bruno Rodrigues Coelho <brkoelho@hotmail.com>
2025-03-02 18:49:23 -0300

    Add files via upload

---
+++ Inicio/desafio_troca.js
@@ -0,0 +1,17 @@
+let a = 7;
+let b = 94;
+
+//Desafio troca de variáveis... a = 94 e b = 7
+
+let c = 94;
+let d = 7;
+a = c;
+b = d;
+console.log(a);
+console.log(b);
+
+//O melhor era usando apenas uma variável temporária.
+
+//O melhor jeito é:
+
+// [a,b]=[b,a]
